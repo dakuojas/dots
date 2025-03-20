fbd86d3d4cb1076752342df922aca9361f009b01
dakuojas <dakuojas@gmail.com>
2025-03-21 02:39:06 +0530

    sync

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -47,6 +47,7 @@ This function should only modify configuration layer settings."
      emacs-lisp
      git
      helm
+     html
      javascript
      lsp
      markdown
@@ -271,8 +272,8 @@ It should only modify the values of Spacemacs settings."
    ;; fixed-pitch faces. The `:size' can be specified as
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
-   dotspacemacs-default-font '("Source Code Pro"
-                               :size 10.0
+   dotspacemacs-default-font '("Hack Nerd Font"
+                               :size 12.0
                                :weight normal
                                :width normal)
 
